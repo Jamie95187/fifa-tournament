abfb86115ba9d19f8a14de67ffd54c57f11a1e12
Jamie <jamiewong63@googlemail.com>
2021-10-22 01:17:07 +0100

    initialise firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import "firebase/firestore";
+
+const config = {
+  apiKey: "AIzaSyB-ucTZG5AGd7RA67XqETWbT2bJgYk1vRE",
+  authDomain: "vue-fifa.firebaseapp.com",
+  projectId: "vue-fifa",
+  storageBucket: "vue-fifa.appspot.com",
+  messagingSenderId: "963994682548",
+  appId: "1:963994682548:web:5a211b47a475be3487c3e1",
+  measurementId: "G-2VV343QB70"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+export const db = firebaseApp.firestore();
