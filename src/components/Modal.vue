062db36fc490580a90bff434883939a8dc070f8f
Jamie <jamiewong63@googlemail.com>
2021-10-23 17:42:31 +0100

    add styling for modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -5,7 +5,7 @@
         <div v-show="modalActive" class="modal-inner">
           <i @click="close" class="fas fa-times-circle"></i>
           <slot />
-          <button>Add Player</button>
+          <button class="confirmButton">Confirm</button>
           <button @click="close" type="button">Cancel</button>
         </div>
       </transition>
@@ -27,4 +27,33 @@ export default {
 </script>
 
 <style>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.7);
+}
+.modal-inner {
+  position: relative;
+  max-width: 640px;
+  width: 80%;
+  background: whitesmoke;
+  padding: 64px 16px;
+}
+i {
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  font-size: 20px;
+  cursor: pointer;
+}
+.confirmButton {
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
 </style>
