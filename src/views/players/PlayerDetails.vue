14aa8b2508fbe2095b85227d1bdbcc93fc36a0de
Jamie <jamiewong63@googlemail.com>
2021-10-29 19:33:43 +0100

    user can delete player from players db

--- src/views/players/PlayerDetails.vue
+++ src/views/players/PlayerDetails.vue
@@ -1,11 +1,21 @@
 <template>
   <h1>Player Details Page</h1>
   <p>This is {{ name }} with id {{ id }}</p>
+  <button @click="deletePlayer">Delete</button>
 </template>
 
 <script>
+import { db } from "../../firebase";
+import router from "../../router/index";
+
 export default {
   props: ["name", "id"],
+  methods: {
+    deletePlayer() {
+      db.collection("players").doc(this.id).delete();
+      router.push({ name: "players" });
+    },
+  },
 };
 </script>
 
